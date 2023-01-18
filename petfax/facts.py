f167aedcc72e94b51c057b30e9647f0035eef866
dany ramirez <drz0224@gmail.com>
2023-01-18 01:56:41 -0800

    fixing image issues

--- petfax/facts.py
+++ petfax/facts.py
@@ -7,17 +7,21 @@ bp = Blueprint('fact', __name__, url_prefix="/facts")
 def new(): 
     return render_template('facts/new.html')
 
-@bp.route("/" , methods=['GET', 'POST'])
+@bp.route("/" , methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        submitter= request.form["submitter"]
-        fact = request.form["fact"]
-
-        new_fact = models.Fact(submitter = submitter, fact = fact)
-        models.db.session.add(new_fact)
-        models.db.session.commit()
-
+        print(request.form)
         return redirect('/facts')
-    results = models.Fact.query.all()
 
-    return render_template('facts/index.html', facts = results)
+    return render_template('facts/index.html')
+    #     submitter= request.form["submitter"]
+    #     fact = request.form["fact"]
+
+    #     new_fact = models.Fact(submitter = submitter, fact = fact)
+    #     models.db.session.add(new_fact)
+    #     models.db.session.commit()
+
+    #     return redirect('/facts')
+    # results = models.Fact.query.all()
+
+    # return render_template('facts/index.html', facts = results)
